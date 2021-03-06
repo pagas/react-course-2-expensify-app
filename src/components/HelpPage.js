808d212c5eaf5b95dfd093be82355170a2ff6df7
paulius <paulius@2simple.com>
2021-03-06 13:37:55 +0000

    remove help page

--- src/components/HelpPage.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const HelpExpensePage = () => (
-    <div>Help dashboard page</div>
-)
-
-export default HelpExpensePage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,6 @@ import { createBrowserHistory } from 'history';
 import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
 import EditExpensePage from "../components/EditExpensePage";
 import AddExpensePage from "../components/AddExpensePage";
-import HelpExpensePage from "../components/HelpPage";
 import NotFoundPage from "../components/NotFoundPage";
 import LoginPage from "../components/LoginPage";
 import PrivateRoute from './PrivateRoute'
@@ -20,7 +19,6 @@ const AppRouter = () => (
                 <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/>
                 <PrivateRoute path="/create" component={AddExpensePage}/>
                 <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
-                <Route path="/help" component={HelpExpensePage}/>
                 <Route component={NotFoundPage}/>
             </Switch>
         </div>
