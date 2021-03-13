2ff4569367131508a959a5a2b27c6b95c5a0f4f5
paulius <paulius@2simple.com>
2021-03-13 04:27:52 +0000

    new style button link, and new style for page header

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import expenseTotal from '../selectors/expense-total';
 import numeral from 'numeral';
@@ -7,8 +8,16 @@ import numeral from 'numeral';
 export const ExpenseSummary = ({expenseCount = 0, expenseTotal}) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses'
     return (
-    <div>
-        <h3>Viewing {expenseCount} {expenseWord} totalling {numeral(expenseTotal / 100).format('$0,0.00')}</h3>
+    <div className="page-header">
+        <div className="content-container">
+            <h1 className="page-header__title">
+                Viewing <span>{expenseCount}</span> {expenseWord} totalling
+                <span> {numeral(expenseTotal / 100).format('$0,0.00')}</span>
+            </h1>
+            <div className="page-header__actions">
+                <Link className="button" to="/create">Add expense</Link>
+            </div>
+        </div>
     </div>
     )
 };
